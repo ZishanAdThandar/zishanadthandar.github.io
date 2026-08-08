8cc5de91abbe74380d0f71a1b9902a087579bdb8
ZishanAdThandar <zishanahamedthandar@gmail.com>
2026-08-08 12:03:46 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   about/404.html
    	new file:   about/CV.pdf
    	new file:   about/README.md
    	new file:   about/assets/css/style.css
    	new file:   about/assets/js/particles.custom.js
    	new file:   about/img/CERT/ardent.jpg
    	new file:   about/img/CERT/bosch1.jpg
    	new file:   about/img/CERT/bosch2.jpg
    	new file:   about/img/CERT/c3sa.png
    	new file:   about/img/CERT/cambridge2022.jpg
    	new file:   about/img/CERT/crta.jpg
    	new file:   about/img/CERT/eccouncil1.jpg
    	new file:   about/img/CERT/geeksforgeeks.jpg
    	new file:   about/img/CERT/hs.jpg
    	new file:   about/img/CERT/madhyamik.jpg
    	new file:   about/img/CERT/makaut.jpg
    	new file:   about/img/CERT/nsdc.jpg
    	new file:   about/img/CERT/paytm.jpg
    	new file:   about/img/CERT/peritusburpmastery.jpg
    	new file:   about/img/CERT/shaadi.jpg
    	new file:   about/img/certification/ardent.png
    	new file:   about/img/certification/bosch.png
    	new file:   about/img/certification/cambridge.png
    	new file:   about/img/certification/codecademy.png
    	new file:   about/img/certification/eccouncil.png
    	new file:   about/img/certification/geeksforgeeks.png
    	new file:   about/img/certification/makaut.png
    	new file:   about/img/certification/nsdc.png
    	new file:   about/img/certification/paytm.png
    	new file:   about/img/certification/shaadi.png
    	new file:   about/img/certification/sololearn.png
    	new file:   about/img/certification/wbhse.png
    	new file:   about/img/certification/wbse.png
    	new file:   about/img/coding/css3Icon.png
    	new file:   about/img/coding/html5Icon.png
    	new file:   about/img/coding/javaIcon.png
    	new file:   about/img/coding/jsIcon.jpg
    	new file:   about/img/coding/mysqlIcon.png
    	new file:   about/img/coding/phpIcon.png
    	new file:   about/img/coding/pythonIcon.png
    	new file:   about/img/contact/mailIcon.png
    	new file:   about/img/contact/msgIcon.png
    	new file:   about/img/contact/whatsAppIcon.png
    	new file:   about/img/hof/alibaba.png
    	new file:   about/img/hof/aol.png
    	new file:   about/img/hof/eur.png
    	new file:   about/img/hof/finefriends.png
    	new file:   about/img/hof/google.png
    	new file:   about/img/hof/hively.png
    	new file:   about/img/hof/mailru.png
    	new file:   about/img/hof/mi.png
    	new file:   about/img/hof/nciipc.png
    	new file:   about/img/hof/oracle.png
    	new file:   about/img/hof/postnl.png
    	new file:   about/img/hof/visenze.png
    	new file:   about/img/hof/zoho.png
    	new file:   about/img/logo.jpg
    	new file:   about/img/logoBig
    	new file:   about/img/og.png
    	new file:   about/img/rahgreen.jpeg
    	new file:   about/img/social/hackeroneIcon.png
    	new file:   about/img/social/htbIcon.png
    	new file:   about/img/social/tryhackme.png
    	new file:   about/img/work/cfssjune.png
    	new file:   about/index.html

---
+++ about/assets/js/particles.custom.js
@@ -0,0 +1,20 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 100, "density": { "enable": true, "value_area": 1000 } },
+    "color": { "value": ["#22c55e", "#facc15", "#38bdf8", "#a855f7"] },
+    "shape": { "type": "circle", "stroke": { "width": 0, "color": "#fff" } },
+    "opacity": { "value": 0.5, "random": false },
+    "size": { "value": 2, "random": true },
+    "line_linked": { "enable": true, "distance": 130, "color": "#ffffff", "opacity": 0.7, "width": 2 }
+  },
+  "interactivity": {
+    "detect_on": "window",
+    "events": { "onhover": { "enable": true, "mode": "grab" }, "onclick": { "enable": true }, "resize": true },
+    "modes": {
+      "grab": { "distance": 150, "line_linked": { "opacity": 1 } }
+    }
+  },
+  "retina_detect": true
+});
+
+
